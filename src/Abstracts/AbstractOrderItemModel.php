68a97090155ccd2120c991a6be0a18374334813c
Martin Svoboda <martin@wpify.io>
2022-08-15 19:27:45 +0200

    Fix Get VAT rate on OrderItem

--- src/Abstracts/AbstractOrderItemModel.php
+++ src/Abstracts/AbstractOrderItemModel.php
@@ -109,25 +109,22 @@ abstract class AbstractOrderItemModel extends AbstractModel {
 	 * @return float
 	 */
 	public function get_vat_rate(): float {
-		static $rate;
-		if ( ! $rate ) {
-			$rate = 0;
+		$rate = 0;
 
-			if ( $this->source_object()->get_tax_status() == 'taxable' ) {
-				$item_data = $this->source_object()->get_data();
-				foreach ( $item_data['taxes']['total'] as $item_tax_id => $item_tax_total ) {
-					$used_item_tax_id = $item_tax_total ? $item_tax_id : null;
-					foreach ( $this->source_object()->get_order()->get_items( 'tax' ) as $item_tax ) {
-						$tax_data = $item_tax->get_data();
-						if ( $tax_data['rate_id'] === $used_item_tax_id ) {
-							$rate = $tax_data['rate_percent'];
-						}
+		if ( $this->source_object()->get_tax_status() == 'taxable' ) {
+			$item_data = $this->source_object()->get_data();
+			foreach ( $item_data['taxes']['total'] as $item_tax_id => $item_tax_total ) {
+				$used_item_tax_id = $item_tax_total ? $item_tax_id : null;
+				foreach ( $this->source_object()->get_order()->get_items( 'tax' ) as $item_tax ) {
+					$tax_data = $item_tax->get_data();
+					if ( $tax_data['rate_id'] === $used_item_tax_id ) {
+						$rate = $tax_data['rate_percent'];
 					}
 				}
-				if ( ! $rate ) {
-					if ( $this->source_object()->get_total_tax() ) {
-						$rate = \round( $this->source_object()->get_total_tax() / ( $this->source_object()->get_total() / 100 ) );
-					}
+			}
+			if ( ! $rate ) {
+				if ( $this->source_object()->get_total_tax() ) {
+					$rate = \round( $this->source_object()->get_total_tax() / ( $this->source_object()->get_total() / 100 ) );
 				}
 			}
 		}
